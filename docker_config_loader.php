c3ec8781006bf008b441c96907de6684b6fe9147
Kai Gohegan <kai@gohegan.uk>
2021-11-20 11:19:44 +0000

    Fixed missing config file

--- docker_config_loader.php
+++ docker_config_loader.php
@@ -4,12 +4,25 @@ foreach(glob("/config/*.php") as $c)
 {
     // Is this a Z-Push config file?
     $matches = [];
+    $destination = null;
     if($config = preg_match("/(?<dir>\/config\/)(?<category>.+).(?<filename>config.php)/", $c, $matches))
     {
-        $destination = "/usr/share/nginx/z-push/backend/".$matches["category"]."/".$matches["filename"];
+        switch($matches["category"])
+        {
+            case "z-push":
+            case "zpush":
+                $destination = "/usr/share/nginx/z-push/".$matches["filename"];
+                break;
+
+            default:
+                $destination = "/usr/share/nginx/z-push/backend/".$matches["category"]."/".$matches["filename"];
+                break;
+        }
+
         if(!copy($c, $destination))
         {
             echo "Warning! Failed to copy '".$c."' to '".$destination."'...\r\n";
         }
+
     }
 }
